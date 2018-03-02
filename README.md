8303a4f3594788106eab62b809b4cfccc170074f
Michael Ding <yandy.ding@gmail.com>
2018-03-02 14:02:40 +0800

    init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# [Home Page](https://shanbay.github.io/peeweext)
+
+[![](http://img.shields.io/travis/shanbay/peeweext.svg?style=flat-square)](https://travis-ci.org/shanbay/peeweext)
+[![](http://img.shields.io/codeclimate/github/shanbay/peeweext.svg?style=flat-square)](https://codeclimate.com/github/shanbay/peeweext)
+[![](https://img.shields.io/coveralls/shanbay/peeweext/master.svg)](https://coveralls.io/github/shanbay/peeweext?branch=master)
+[![](https://img.shields.io/pypi/v/peeweext.svg)](https://github.com/shanbay/peeweext)
+[![](https://img.shields.io/pypi/pyversions/peeweext.svg)](https://github.com/shanbay/peeweext)
+[![](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://shanbay.mit-license.org)
---
+++ setup.py
@@ -0,0 +1,64 @@
+import os
+import re
+import ast
+from setuptools import setup, find_packages
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+_root = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(_root, 'peeweext/__init__.py')) as f:
+    version = str(ast.literal_eval(_version_re.peeweextrch(
+        f.read()).group(1)))
+
+with open(os.path.join(_root, 'requirements.txt')) as f:
+    requirements = f.readlines()
+
+with open(os.path.join(_root, 'README.md')) as f:
+    readme = f.read()
+
+
+def find_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return filepaths
+
+
+setup(
+    name='peeweext',
+    version=version,
+    description='peewee extension',
+    long_description=readme,
+    url='https://github.com/shanbay/peeweext',
+    author='Michael Ding',
+    author_email='yandy.ding@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords=['peewee', 'orm'],
+    packages=find_packages(exclude=['tests']),
+    package_data={'peeweext': find_package_data('peeweext')},
+    python_requires='>=3',
+    install_requires=requirements
+)
