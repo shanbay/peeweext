825c4f23c83f9b68303f6dd1521c5d7802f351d4
Michael Ding <yandy.ding@gmail.com>
2018-03-08 19:32:02 +0800

    add ts

--- peeweext/__init__.py
+++ peeweext/__init__.py
@@ -1,8 +1,10 @@
+import pendulum
+import datetime
 import peewee as pw
-from playhouse.db_url import connect, register_database
-from playhouse import pool
+from blinker import signal
+from playhouse import pool, db_url
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 try:
     from sea.utils import import_string, cached_property
@@ -18,7 +20,7 @@ class Peeweext:
     def init_app(self, app):
         config = app.config.get_namespace(self.ns)
         conn_params = config.get('conn_params', {})
-        self.database = connect(config['db_url'], **conn_params)
+        self.database = db_url.connect(config['db_url'], **conn_params)
         self.model_class = import_string(config.get('model', 'peeweext.Model'))
 
     @cached_property
@@ -28,22 +30,80 @@ class Peeweext:
                 database = self.database
         return BaseModel
 
-    def __getattr__(self, name):
-        return getattr(self.database, name)
+
+class DatetimeTZField(pw.Field):
+    field_type = 'DATETIME'
+
+    def python_value(self, value):
+        if isinstance(value, str):
+            return pendulum.parse(value)
+        if isinstance(value, datetime.datetime):
+            return pendulum.instance(value)
+        return value
+
+    def db_value(self, value):
+        if value is None:
+            return value
+        if not isinstance(value, datetime.datetime):
+            raise ValueError('datetime instance required')
+        if value.utcoffset() is None:
+            raise ValueError('timezone aware datetime required')
+        if isinstance(value, pendulum.Pendulum):
+            value = value._datetime
+        return value.astimezone(datetime.timezone.utc)
+
+
+pre_save = signal('pre_save')
+post_save = signal('post_save')
+pre_delete = signal('pre_delete')
+post_delete = signal('post_delete')
+pre_init = signal('pre_init')
 
 
 class Model(pw.Model):
-    pass
+
+    created_at = DatetimeTZField(default=pendulum.utcnow)
+    updated_at = DatetimeTZField(default=pendulum.utcnow)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pre_init.send(type(self), instance=self)
+
+    def save(self, *args, **kwargs):
+        pk_value = self._pk
+        created = kwargs.get('force_insert', False) or not bool(pk_value)
+        pre_save.send(type(self), instance=self, created=created)
+        ret = super().save(*args, **kwargs)
+        post_save.send(type(self), instance=self, created=created)
+        return ret
+
+    def delete_instance(self, *args, **kwargs):
+        pre_delete.send(type(self), instance=self)
+        ret = super().delete_instance(*args, **kwargs)
+        post_delete.send(type(self), instance=self)
+        return ret
+
+
+def _touch_model(sender, instance, created):
+    if issubclass(sender, Model):
+        instance.updated_at = pendulum.utcnow()
+
+
+pre_save.connect(_touch_model)
+pw.MySQLDatabase.field_types.update({'DATETIME': 'DATETIME(6)'})
+pw.PostgresqlDatabase.field_types.update({'DATETIME': 'TIMESTAMPTZ'})
 
 
 class SmartDatabase:
 
     """
-    if you use transaction, you must use it in a connection context explict:
+    if you use transaction, you must wrap it with a connection context explict:
 
     with db.connection_context():
         with db.atomic() as transaction:
             pass
+
+    **notice**: if you use nested transactions, only wrap the most outside one
     """
 
     def execute(self, *args, **kwargs):
@@ -66,5 +126,5 @@ for n, urls in _smarts.items():
     pc = getattr(pool, 'Pooled{}'.format(n[5:]))
     if pc is not None:
         smart_cls = type(n, (SmartDatabase, pc), {})
-        register_database(smart_cls, *urls)
+        db_url.register_database(smart_cls, *urls)
         globals()[n] = smart_cls
