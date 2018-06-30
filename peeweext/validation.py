39e02204db21539baf27326024f574d90ec0003b
erliang <dev@earlzo.me>
2018-06-30 17:29:09 +0800

    Added: URLValidator

--- peeweext/validation.py
+++ peeweext/validation.py
@@ -1,6 +1,8 @@
 """Validator for peewee Model"""
 import re
 import functools
+import ipaddress
+from urllib.parse import urlsplit, urlunsplit
 
 
 class ValidationError(Exception):
@@ -52,6 +54,95 @@ class RegexValidator(BaseValidator):
                 'value {:s} not match r"{:s}"'.format(value, self._regex))
 
 
+class URLValidator(RegexValidator):
+    """
+    See https://docs.djangoproject.com/zh-hans/2.0/ref/validators/#urlvalidator
+    """
+
+    ul = '\u00a1-\uffff'  # unicode letters range (must not be a raw string)
+
+    # IP patterns
+    ipv4_re = r'(?:25[0-5]|2[0-4]\d|[0-1]?\d?\d)(?:\.'\
+        '(?:25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}'
+    ipv6_re = r'\[[0-9a-f:\.]+\]'  # (simple regex, validated later)
+
+    # Host patterns
+    hostname_re = r'[a-z' + ul + \
+        r'0-9](?:[a-z' + ul + r'0-9-]{0,61}[a-z' + ul + r'0-9])?'
+    # Max length for domain name labels is 63 characters per RFC 1034 sec. 3.1
+    domain_re = r'(?:\.(?!-)[a-z' + ul + r'0-9-]{1,63}(?<!-))*'
+    tld_re = (
+        r'\.'  # dot
+        r'(?!-)'  # can't start with a dash
+        r'(?:[a-z' + ul + '-]{2,63}'  # domain label
+        r'|xn--[a-z0-9]{1,59})'  # or punycode label
+        r'(?<!-)'  # can't end with a dash
+        r'\.?'  # may have a trailing dot
+    )
+    host_re = '(' + hostname_re + domain_re + tld_re + '|localhost)'
+
+    _compiled_regex = re.compile(
+        r'^(?:[a-z0-9\.\-\+]*)://'  # scheme is validated separately
+        r'(?:\S+(?::\S*)?@)?'  # user:pass authentication
+        r'(?:' + ipv4_re + '|' + ipv6_re + '|' + host_re + ')'
+        r'(?::\d{2,5})?'  # port
+        r'(?:[/?#][^\s]*)?'  # resource path
+        r'\Z', re.IGNORECASE)
+    _regex = _compiled_regex.pattern
+    message = 'Enter a valid URL.'
+    schemes = ['http', 'https', 'ftp', 'ftps']
+
+    def __init__(self, schemes=None, null=False):
+        self.null = null
+        if schemes is not None:
+            self.schemes = schemes
+
+    def validate(self, value):
+        if self.null and not value:
+            return
+        # Check first if the scheme is valid
+        scheme = value.split('://')[0].lower()
+        if scheme not in self.schemes:
+            raise ValidationError(self.message)
+
+        # Then check full URL
+        try:
+            super().validate(value)
+        except ValidationError as e:
+            # Trivial case failed. Try for possible IDN domain
+            if value:
+                try:
+                    scheme, netloc, path, query, fragment = urlsplit(value)
+                except ValueError:  # for example, "Invalid IPv6 URL"
+                    raise ValidationError(self.message)
+                try:
+                    netloc = netloc.encode('idna').decode(
+                        'ascii')  # IDN -> ACE
+                except UnicodeError:  # invalid domain part
+                    raise e
+                url = urlunsplit((scheme, netloc, path, query, fragment))
+                super().validate(url)
+            else:
+                raise
+        else:
+            # Now verify IPv6 in the netloc part
+            host_match = re.search(
+                r'^\[(.+)\](?::\d{2,5})?$', urlsplit(value).netloc)
+            if host_match:
+                potential_ip = host_match.groups()[0]
+                try:
+                    ipaddress.IPv6Address(potential_ip)
+                except ValueError:
+                    raise ValidationError(self.message)
+
+        # The maximum length of a full host name is 253 characters per RFC 1034
+        # section 3.1. It's defined to be 255 bytes or less, but this includes
+        # one byte for the length of the name and one byte for the trailing dot
+        # that's used to indicate absolute names in DNS.
+        if len(urlsplit(value).netloc) > 253:
+            raise ValidationError(self.message)
+
+
 class LengthValidator(BaseValidator):
     def __init__(self, min_length, max_length):
         self.min_length = min_length
@@ -72,6 +163,7 @@ class validates:
 
     :param args: Validator objects
     """
+
     def __init__(self, *args):
         self.validators = list(args)
 
