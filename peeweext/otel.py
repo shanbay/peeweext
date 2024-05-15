60fe5c4714181d8f571b27ac63af720d242dd155
xuhaidong <haidong.xu@shanbay.com>
2024-05-15 16:46:51 +0800

    feat: compatible with mysql clients

--- peeweext/otel.py
+++ peeweext/otel.py
@@ -1,5 +1,27 @@
+from enum import Enum
 from opentelemetry import trace
-from opentelemetry.instrumentation.mysqlclient import MySQLClientInstrumentor
+
+mysql_connector = None
+
+
+class MySQLConnector(Enum):
+    mysqldb = 0
+    pymysql = 1
+
+
+try:
+    import MySQLdb
+    from opentelemetry.instrumentation.mysqlclient import MySQLClientInstrumentor
+
+    mysql_connector = MySQLConnector.mysqldb
+except ImportError:
+    try:
+        import pymysql
+        from opentelemetry.instrumentation.pymysql import PyMySQLInstrumentor
+
+        mysql_connector = MySQLConnector.pymysql
+    except ImportError:
+        pass
 
 
 def sync_once(func):
@@ -14,7 +36,11 @@ def sync_once(func):
 
 @sync_once
 def otel_instrument(app=None):
-    if app is None or app.config.get_namespace("OTEL_").get("enable", False):
-        MySQLClientInstrumentor().instrument(
-            tracer_provider=trace.get_tracer_provider()
-        )
+    if app is not None and not app.config.get_namespace("OTEL_").get("enable", False):
+        return
+
+    tp = trace.get_tracer_provider()
+    if mysql_connector == MySQLConnector.mysqldb:
+        MySQLClientInstrumentor().instrument(tracer_provider=tp)
+    elif mysql_connector == MySQLConnector.pymysql:
+        PyMySQLInstrumentor().instrument(tracer_provider=tp)
